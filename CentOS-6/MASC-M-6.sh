48723e2de0fab1d360b049d0aee984943ab04c7e
magenx <designelo@gmail.com>
2015-10-27 16:01:29 +0000

    Update MASC-M-6.sh

--- CentOS-6/MASC-M-6.sh
+++ CentOS-6/MASC-M-6.sh
@@ -5,13 +5,13 @@
 #       All rights reserved.                                         #
 #====================================================================#
 SELF=$(basename $0)
-MASCM_VER="6.9.2a"
+MASCM_VER="6.9.5"
 
 # Software versions 
 #MAGE_LATEST=$(wget -q -O- http://connect20.magentocommerce.com/community/Mage_All_Latest/releases.xml | tail -6 | grep -Po '(?<=<v>).*(?=</v>)')
-MAGENTO_TMP_FILE="https://www.dropbox.com/s/fo8tq9wdgozhxbt/magento-1.9.2.1-2015-08-03-06-35-45.tar.gz"
-MAGENTO_VER="1.9.2.1"
-PHPMYADMIN_VER="4.4.13.1"
+MAGENTO_TMP_FILE="https://www.dropbox.com/s/v6libswo5zd68q2/magento-1.9.2.2-2015-10-27-03-19-32.tar.gz"
+MAGENTO_VER="1.9.2.2"
+PHPMYADMIN_VER="4.5.1"
 
 # Simple colors
 RED="\e[31;40m"
@@ -748,7 +748,7 @@ if [ "${new_down}" == "y" ];then
         echo -n "      DOWNLOADING MAGENTO  "
         long_progress &
         pid="$!"
-        wget -qO- ${MAGENTO_TMP_FILE} | tar -xzp --strip 1
+        wget -qO- ${MAGENTO_TMP_FILE}
         stop_progress "$pid"
         echo
         else
