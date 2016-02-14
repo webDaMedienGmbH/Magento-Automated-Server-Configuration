2223909eaedf5eb5eb67cd99fb9392be8ac67dc5
magenx <magenx@users.noreply.github.com>
2016-02-14 15:23:10 +0000

    Update MASC-M-7-v2.sh

--- Magento-2/CentOS-7/MASC-M-7-v2.sh
+++ Magento-2/CentOS-7/MASC-M-7-v2.sh
@@ -26,7 +26,7 @@ REPO_EPEL="http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noar
 REPO_PERCONA="http://www.percona.com/redir/downloads/percona-release/redhat/latest/percona-release-0.1-3.noarch.rpm"
 REPO_NGINX="http://nginx.org/packages/mainline/centos/7/x86_64/"
 REPO_REMI="http://rpms.famillecollet.com/enterprise/remi-release-7.rpm"
-REPO_HHVM="https://yum.gleez.com/7/x86_64/hhvm-3.10.1-1.centos.x86_64.rpm"
+REPO_HHVM="https://yum.gleez.com/7/x86_64/hhvm-3.12.0-1.el7.centos.x86_64.rpm"
 
 # WebStack Packages
 EXTRA_PACKAGES="boost tbb lz4 libyaml libdwarf bind-utils e2fsprogs svn gcc iptraf inotify-tools net-tools mcrypt mlocate unzip vim wget curl sudo bc mailx clamav-filesystem clamav-server clamav-update clamav-milter-systemd clamav-data clamav-server-systemd clamav-scanner-systemd clamav clamav-milter clamav-lib clamav-scanner proftpd logrotate git patch ipset strace rsyslog gifsicle GeoIP ImageMagick libjpeg-turbo-utils pngcrush lsof goaccess net-snmp net-snmp-utils xinetd python-pip ncftp postfix"
